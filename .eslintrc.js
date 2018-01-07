1e478abc3f7b1b67feaf98b28ef143b683fd88df
marlti7 <731550954@qq.com>
2018-01-07 21:30:25 +0800

    first

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+    "react",
+    "react-native"
+    ],
+    "globals": {
+      "__DEV__": true,
+      "fetch": true
+    },
+    "parser": "babel-eslint",
+    "rules": {
+      "react/prefer-stateless-function": "off",
+      "max-len": ["error", 120],
+      "no-console": 0,
+      "react/forbid-prop-types": [0, { "forbid": ["any", "array", "object"] }],
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    }
+};
