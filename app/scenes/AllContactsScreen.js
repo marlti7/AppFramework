f58cc15011cc118e319d5b95ae7f8d2c17a646c7
marlti7 <731550954@qq.com>
2018-02-09 09:46:34 +0800

    测试fetch 豆瓣api

--- app/scenes/AllContactsScreen.js
+++ app/scenes/AllContactsScreen.js
@@ -5,7 +5,25 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+const GET_URL = 'https://api.douban.com/v2/movie/top250';
+
 export default class AllContactsScreen extends React.Component {
+  componentDidMount() {
+    this.fetchData();
+  }
+  fetchData = () => {
+    fetch(GET_URL)
+    // ES6的写法左边代表输入的参数右边是逻辑处理和返回结果
+      .then(response => response.json())
+      .then((responseData) => {
+        console.log("responseData", responseData);
+
+        // this.setState({
+        //   data: responseData,
+        // });
+      })
+      .done();
+  }
   render() {
     const { navigation } = this.props;
     return <Text>List of all contacts</Text>;
